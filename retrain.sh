07987d5bd373f387d2ef530d05d0251a64020be8
Gonda Gábor <48316403+Gazuru@users.noreply.github.com>
2021-07-16 09:30:16 -0700

    Shell Script for Linux AI network retrain

---
+++ retrain.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd ~
+
+mkdir -p train_images
+mkdir -p train_images/correct
+mkdir -p train_images/faulty
+
+read -n 1 -p "If you didn't place your training images in the train_images' proper folder press Ctrl+C, if you did, press any button."
+
+if [ ! -d /etc/pki/tls/certs ] && [ ! -a /etc/pki/tls/certs/ca-bundle.crt ] ; then
+	sudo mkdir -p /etc/pki/tls/certs
+	sudo cp /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt
+fi
+
+if [[ -z $(which conda) ]] ; then
+	wget -O Anaconda3-4.0.0-Linux-x86_64.sh http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+	wait
+	bash Anaconda3-4.0.0-Linux-x86_64.sh -b
+	wait
+fi
+
+wait
+
+conda update conda -y
+wait
+if [[ -z $(conda env list | grep venv_x) ]] ; then
+	conda create -n venv_x python=3.6 -y
+fi
+wait
+source activate venv_x
+wget -O model_retrain.ipynb https://raw.githubusercontent.com/Gazuru/xeonsoft_project/master/model_retrain.ipynb
+wait
+ipython -c "%run model_retrain.ipynb"
+wait
+cd tensorflow-for-poets-2/tf_files
+echo "A retrained_graph.binaryproto filera lesz szuksege"
+source deactivate
