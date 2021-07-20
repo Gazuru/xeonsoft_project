8113146d93f642b43ee531b1b7922894e4bca36d
Gonda Gábor <48316403+Gazuru@users.noreply.github.com>
2021-07-20 00:01:04 -0700

    Végleges verziók feltöltése

--- shell_scripts/upload.sh
+++ shell_scripts/upload.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-[[ -d ~/tensorflow-for-poets-2 ]] || echo "Please run 'retrain.sh' before this script."; exit 1;
+if [[ ! -d ~/tensorflow-for-poets-2 ]] ; then
+	echo "Please run 'retrain.sh' before this script."
+       	exit 1;
+fi
 
 cd ~/tensorflow-for-poets-2/tf_files
 
-[[ ! -z "$1" ]] && address=$1 || read -p "Please enter the IP address of the Pi you would like to upload this network to: " address
+if [[ ! -z "$1" ]] ; then
+       	address=$1 
+else
+	read -p "Please enter the IP address of the Pi you would like to upload this network to: " address
+fi
 
 while [[ ! $address =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}$ ]] ; do
 	read -p "Invalid IP address format. Please try again: " address
 done
 
 scp retrained_* pi@"$address":/home/pi
-[[ $? -eq 0 ]] && echo "File transferred successfully. Now you can run 'cleanup.sh'."
+if [[ $? -eq 0 ]] ; then
+       	echo "File transferred successfully. Now you can run 'cleanup.sh'."
+fi
 
